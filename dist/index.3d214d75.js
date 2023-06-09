49b7c6369768ee0e82295e1384be376700826c33
guledm12 <guledmo12@gmail.com>
2023-06-08 21:16:37 -0400

    modified slight bug in back-end & added a truncate text function

--- dist/index.3d214d75.js
+++ dist/index.3d214d75.js
@@ -579,23 +579,33 @@ var _api = require("./api");
 var _apiDefault = parcelHelpers.interopDefault(_api);
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
+// Form event listener
 searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    // get search term
     const searchTerm = searchInput.value;
+    //get sort
     const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+    // get limit
     const searchLimit = document.getElementById("limit").value;
+    // check input if empty
     if (searchTerm === "") showMessage("Please add a search term", "alert-danger");
+    // to clear input after search
     searchInput.value = "";
-    reddit.search(searchTerm, searchLimit, sortBy).then((results)=>{
+    (0, _apiDefault.default).search(searchTerm, searchLimit, sortBy).then((results)=>{
         let output = '<div class="card-columns">';
         // loop through post
         results.forEach((post)=>{
             output += `<div class="card">
-            <div class="card-body">
-            <h5 class="card-title">${post.title}</h5>
-            <p class="card-text">${truncateText(post.selftext, 100)}</p>
-            <a href="${post.url}" target="_blank" class="btn btn-dark">Read More</a>
-            </div> 
-            </div>`;
+     <div class="card-body">
+  
+      <h5 class="card-title">${post.title}</h5>
+  
+      <p class="card-text">${truncateText(post.selftext, 100)}</p>
+  
+      <a href="${post.url}" target="_blank" class="btn btn-dark">Read More</a> 
+    </div>
+  </div>`;
         });
         output += "</div>";
         document.getElementById("results").innerHTML = output;
--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 import api from "./api";
-
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
 
-
+// Form event listener
 searchForm.addEventListener("submit", (e) => {
-    const searchTerm = searchInput.value;
-    const sortBy = document.querySelector('input[name="sortby"]:checked').value;
-    const searchLimit = document.getElementById("limit").value;
+  e.preventDefault();
+  // get search term
+  const searchTerm = searchInput.value;
+  //get sort
+  const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+  // get limit
+  const searchLimit = document.getElementById("limit").value;
+  // check input if empty
+  if (searchTerm === "") {
+    showMessage("Please add a search term", "alert-danger");
+  }
+  // to clear input after search
+  searchInput.value = "";
+  api.search(searchTerm, searchLimit, sortBy).then((results) => {
+    let output = '<div class="card-columns">';
+    // loop through post
+    results.forEach((post) => {
 
-    if (searchTerm === "") {
-      showMessage("Please add a search term", "alert-danger");
-    }
-    searchInput.value = "";
-    reddit.search(searchTerm, searchLimit, sortBy).then((results) => {
-        let output = '<div class="card-columns">';
-        results.forEach((post) => {
-          output += `<div class="card">
-                <div class="card-body">
-                <h5 class="card-title">${post.title}</h5>
-                <p class="card-text">${truncateText(post.selftext, 100)}</p>
-                <a href="${post.url}" target="_blank" class="btn btn-dark">Read More</a>
-                </div> 
-                </div>`
-        ;});
-        output += "</div>";
-        document.getElementById("results").innerHTML = output;
-      });
+      output += `<div class="card">
+     <div class="card-body">
+  
+      <h5 class="card-title">${post.title}</h5>
+  
+      <p class="card-text">${truncateText(post.selftext, 100)}</p>
+  
+      <a href="${post.url}" target="_blank" class="btn btn-dark">Read More</a> 
+    </div>
+  </div>`;
+    });
+    output += "</div>";
+    document.getElementById("results").innerHTML = output;
+  });
 });
+
+function truncateText(text,limit){
+    const shortened =text.indexOf('', limit);
+    if(shortened ==-1)return text;
+    return text.substring(0, shortened);
+}
