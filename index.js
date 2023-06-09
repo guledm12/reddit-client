43d16db373618e83c46abda5eaf439026d11c306
guledm12 <guledmo12@gmail.com>
2023-06-08 22:11:12 -0400

    added subreddit filter input

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const searchInput = document.getElementById("search-input");
 
 // Form event listener
 searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   const searchTerm = searchInput.value;
   const sortBy = document.querySelector('input[name="sortby"]:checked').value;
@@ -12,18 +13,17 @@ searchForm.addEventListener("submit", (e) => {
   if (searchTerm === "") {
     alert("Please add a search term");
   }
-
   api.search(searchTerm, searchLimit, sortBy).then((results) => {
     let output = '<div class="card-columns">';
     results.forEach((post) => {
 
       output += `<div class="card">
-            <div class="card-body">
-            <h5 class="card-title">${post.title}</h5>
-            <p class="card-text">${truncateText(post.selftext, 100)}</p>
-            <a href="${post.url}" target="_blank" class="btn btn-dark">Read More</a> 
-            </div>
-            </div>`;
+        <div class="card-body">
+        <h5 class="card-title">${post.title}</h5>
+        <p class="card-text">${truncateText(post.selftext, 100)}</p>
+        <a href="${post.url}" target="_blank" class="btn btn-dark">Read More</a> 
+        </div>
+        </div>`;
     });
     output += "</div>";
     document.getElementById("results").innerHTML = output;
@@ -33,5 +33,5 @@ searchForm.addEventListener("submit", (e) => {
 function truncateText(text,limit){
     const shortened =text.indexOf('', limit);
     if(shortened ==-1)return text;
-    return text.substring(0, shortened) + "...";
+    return text.substring(0, shortened);
 }
