19cc595c4a2e123b087b8be2e7a3c067bc61badf
guledm12 <guledmo12@gmail.com>
2023-06-19 22:57:27 -0400

    Got the subreddit filter working but leaving it defa ult results in a bug

--- api.js
+++ api.js
@@ -1,9 +1,7 @@
 export default {
-  search: function (searchTerm, searchLimit, sortBy) {
+  search: function (searchTerm, subreddit, searchLimit, sortBy, restrict) {
     return (
-      fetch(
-        `http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
-      )
+      fetch(`http://www.reddit.com/${subreddit}/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}${restrict}`)
         .then((res) => res.json())
         .then((data) => data.data.children.map((data) => data.data))
         .catch((err) => console.log(err))
--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 import api from "./api";
+
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
+const subredditFilter = document.getElementById("subreddit-filter");
 
 // Form event listener
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const searchTerm = searchInput.value;
+  const subreddit = "r/" + subredditFilter.value;
   const sortBy = document.querySelector('input[name="sortby"]:checked').value;
   const searchLimit = document.getElementById("limit").value;
 
+  let restrict;
+  if (subredditFilter.value != "") {
+    restrict = "&restrict_sr=1"
+  } else {
+    restrict = ""
+  }
+
+  console.log(restrict)
   if (searchTerm === "") {
     alert("Please add a search term");
   }
-  api.search(searchTerm, searchLimit, sortBy).then((results) => {
+  api.search(searchTerm, subreddit, searchLimit, sortBy, restrict).then((results) => {
     let output = '<div class="card-columns">';
     results.forEach((post) => {
 
